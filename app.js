b098e88671f027498c77d609af8f1b3233ebf5af
eve712 <62237639+eve712@users.noreply.github.com>
2021-02-22 21:25:54 +0900

    Server: 익스프레스 서버 설치
    * 익스프레스 서버 설치
    * node-sass 설치
    * package.json에 반영

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/index.html'));
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중')
+});
