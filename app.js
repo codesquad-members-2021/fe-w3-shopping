9c59437132bbe361500891c6ea060ad61107439d
17-sss <xzxking17@gmail.com>
2021-02-22 16:57:55 +0900

    initial commit - index.ejs 초기 구조 작성

---
+++ app.js
@@ -0,0 +1,48 @@
+// -- express generator 사용
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+require('dotenv').config(); // dotenv 설정
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+// 뷰 엔진 설정 및 서버 포트 설정
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('port', process.env.SERVER_PORT || 4000);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`http://localhost:${app.get('port')}`);
+});
+
+module.exports = app;
