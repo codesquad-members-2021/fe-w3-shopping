a44d2ffb9974be17caee2f3a0d41a82ea3429ce8
Jaeuk-YOO <ryou9305@naver.com>
2021-02-22 18:36:37 +0900

    [ADD] json require 작업 시도

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,24 @@
 import express from "../node_modules/express/lib/express.js";
+import cors from "../node_modules/cors/lib/index.js";
+import { homeContents, planningEvents } from "./routes.js"; // 임시
 
 // var express = require('express');
+// const cors = require('cors')
+
 const app = express();
 const port = 3033;
+
+app.use(cors());
+// app.use(express.static(`${__dirname}/public`))
+// app.use('/api', routes)
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.get('/homeContents', (req, res) => {
-
+  // console.log(homeContents)
 })
 
 app.listen(port, () => {
