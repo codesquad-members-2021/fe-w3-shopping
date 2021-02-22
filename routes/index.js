b5c59cddb2b15f4751fad46f5de376c7bba255df
SEONKYU\difke <somedaycode@gmail.com>
2021-02-22 22:19:10 +0900

    ejs 템플릿 샘플 코드를 시험함

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index.ejs');
+  res.render('index');
 });
 
 module.exports = router;
