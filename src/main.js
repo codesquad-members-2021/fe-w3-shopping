269b7c6255e8f8e778d1541bce878f5129f8e28b
reminderclock <reminderclock@naver.com>
2021-02-22 17:29:06 +0900

    feat: html get 방식으로 서버에 데이터전송

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ const express = require('express');
 // 불러옴 함수를 실행해줌
 const app = express();
 app.listen(3000, function() {
-    console.log('start! express server on port 3000');
+    console.log('start!!! express server on port 3000');
 });
-
-
 // 비동기로 코드가 진행된다. 
+
+app.get('/', function(req,res) {
+    res.sendFile("/Users/luke/Documents/gitStorage/fe-w3-shopping/index.html")
+});
