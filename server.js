fb37582b5af5a5cc6a71c3dcf41bb54d263d6cff
jjunyjjuny <jjuny546@gmail.com>
2021-02-22 21:56:56 +0900

    Add GET test image by express express로 로컬의 이미지를 GET 요청하고 받는 테스트 완료

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 import express from "express";
 import path from "path";
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT || 8080;
-const ROOT_DIR = path.join(__dirname, "src");
+const SRC_DIR = path.join(__dirname, "src");
+const IMG_DIR = path.join(__dirname, "server_img");
+app.use(express.static(IMG_DIR));
 
 app.get("/", (req, res) => {
-  const indexHTML = path.join(ROOT_DIR, "index.html");
+  const indexHTML = path.join(SRC_DIR, "index.html");
   res.sendFile(indexHTML);
+});
+app.get("/banner/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`TEST, <img src='/banner/${id}.png'>`);
 });
 
 app.listen(PORT, () => {
