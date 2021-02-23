a974a03de55138231b2abbccb36185b85f45ea83
jeonyeonkyu <ehb6915@naver.com>
2021-02-24 01:26:00 +0900

    Add: sass, webpack, nodemon 자동 watch 설정

--- beemo/webpack.config.js
+++ beemo/webpack.config.js
@@ -2,20 +2,9 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: "./public/javascripts/main.js",
+  watch: true,
   output: {
     path: path.resolve(__dirname, "public"),
     filename: 'main_bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
   }
 }
