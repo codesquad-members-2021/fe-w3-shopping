fbeb183f389371499b11c31445738f9955cbce2f
17-sss <xzxking17@gmail.com>
2021-02-23 11:11:58 +0900

    Fix Dir structure change & Creating header style..

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '쇼핑하우 by kakaocommerce' });
+router.get('/', (req, res) => {
+    res.render('index', { title: '쇼핑하우 by kakaocommerce' });
 });
 
 module.exports = router;
