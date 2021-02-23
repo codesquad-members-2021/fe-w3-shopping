24b3e3757d6fe008424db2b39ae61ffd1ada1e72
eve712 <62237639+eve712@users.noreply.github.com>
2021-02-23 21:53:28 +0900

    Add: html 작성 중

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.set('port', 3000);
 // app.set('views', __dirname+ '/views')
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.locals.homeContents = require('./public/data/homeContents.json');
 
