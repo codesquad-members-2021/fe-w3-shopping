ff9e38fbadd696a4f5921ee5d40d62b19a19ebf0
eve712 <62237639+eve712@users.noreply.github.com>
2021-02-23 19:33:41 +0900

    Fix: 폴더 구조 수정, ejs로 변경
    * data 파일 가져옴
    * 폴더 구조 수정
    * 템플릿 작업을 쉽게 하기 위해 html에서 ejs로 변경
    * app.js에서 json 데이터 불러오기

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-app.set('port', process.env.PORT || 3000);
+
+app.set('port', 3000);
+// app.set('views', __dirname+ '/views')
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.locals.homeContents = require('./public/data/homeContents.json');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/src/index.html'));
+    res.render("index", {});
 });
 
 app.listen(app.get('port'), () => {
