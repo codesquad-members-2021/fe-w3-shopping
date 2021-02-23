d911f6a42d86e99121d626bf6936dd940609a7e5
adelakim <diddnjs02@likelion.org>
2021-02-24 00:07:30 +0900

    [Add] Insert responsed json data into carousel
    - main.js
        - 캐러셀에 json data 넣기 진행
        - fetch하고 비동기로 받으니까, json data를 받아와 html 태그를 생성하여 innerHtml로 넣은 후 캐러셀을 조작해야 한다는 것은 알겠는데, 코드가 너무 길어짐
        - 현재 캐러셀 부분을 class로 객체화하거나 따로 분리해서 import할 수 있는 형식으로 만들어야 할 것 같은데 아무 아이디어도 떠오르지 않음.. ㅜㅜ

--- shopping/public/javascripts/main.js
+++ shopping/public/javascripts/main.js
@@ -1,9 +1,12 @@
 const mallEventSlide = document.querySelector("#mallEventSlide");
+const slideList = document.querySelector(".slide_list");
 
-// console.log(mallEventSlide);
-
-const createPanel = (lists) => {
+const createMallEventListPanel = (lists) => {
   return `<div class="panel"><ul class="list_item">${lists}</ul></div>`;
+};
+
+const createCarouselPanel = (href_url, img_url) => {
+  return `<div class="panel"><a href="${href_url}"><img src="${img_url}"></a></div>`;
 };
 
 const createLists = (href_url, img_url, title, info) => {
@@ -19,30 +22,100 @@ const createLists = (href_url, img_url, title, info) => {
   </li>`;
 };
 
-let panels = "";
-let lists = "";
-
 fetch("http://localhost:3000/planningEvent.json")
   .then((res) => res.json())
   .then((data) => {
     const event = data.event; // best100 event 상품
     const mileageList = data.mileageList; // 캐러셀
-    console.log(mileageList);
     const mallEventListProducts = data.mallEventList;
-    console.log(mallEventListProducts);
-    mallEventListProducts.forEach((product, idx, array) => {
-      const { imgurl, linkurl, text, text2 } = product;
-      //   let panels = "";
-      if (idx % 5 === 0) {
-        if (idx) panels += createPanel(lists);
+
+    // mileageList html 코드 만들기
+    const carousel_panels = mileageList.reduce((acc, val) => {
+      const { imgurl, linkurl } = val;
+      acc += createCarouselPanel(linkurl, imgurl);
+      return acc;
+    }, ``);
+
+    // mallEventListProducts html 코드 만들기
+    let lists = "";
+    const mallEventListPanels = mallEventListProducts.reduce((acc, val, idx) => {
+      const { imgurl, linkurl, text, text2 } = val;
+      if (!idx % 5) {
+        if (idx) acc += createMallEventListPanel(lists);
         lists = "";
         lists += createLists(linkurl, imgurl, text, text2);
-      } else if (idx === array.length - 1) {
+      } else if (idx === mallEventListProducts.length - 1) {
         lists += createLists(linkurl, imgurl, text, text2);
-        panels += createPanel(lists);
-      } else {
-        lists += createLists(linkurl, imgurl, text, text2);
+        acc += createMallEventListPanel(lists);
+      } else lists += createLists(linkurl, imgurl, text, text2);
+      return acc;
+    }, ``);
+
+    return { carousel_panels, mallEventListPanels };
+  })
+  .then((res) => {
+    const { carousel_panels, mallEventListPanels } = res;
+    slideList.innerHTML = carousel_panels;
+    console.log(slideList);
+    mallEventSlide.innerHTML = mallEventListPanels;
+
+    const panels = document.querySelectorAll(".panel");
+    const prevButton = document.querySelector(".btn_prev");
+    const nextButton = document.querySelector(".btn_next");
+    const slideLen = panels.length;
+    const slideWidth = 485;
+    const slideSpeed = 300;
+    const startNum = 0; // index 0 ~ 4
+
+    slideList.style.width = `${slideWidth * (slideLen + 2)}px`;
+
+    let firstChild = slideList.firstElementChild;
+    let lastChild = slideList.lastElementChild;
+    let clonedFirst = firstChild.cloneNode(true);
+    let clonedLast = lastChild.cloneNode(true);
+
+    // add cloned slides
+    slideList.appendChild(clonedFirst);
+    slideList.insertBefore(clonedLast, slideList.firstElementChild);
+
+    slideList.style.transform = `translateX(-${slideWidth * (startNum + 1)}px)`;
+
+    let currIndex = startNum;
+    let currSlide = panels[currIndex];
+    currSlide.classList.add("slide_active");
+
+    nextButton.addEventListener("click", () => {
+      if (currIndex <= slideLen - 1) {
+        slideList.style.transition = slideSpeed + "ms";
+        slideList.style.transform = `translateX(-${slideWidth * (currIndex + 2)}px)`;
       }
+      if (currIndex === slideLen - 1) {
+        setTimeout(() => {
+          slideList.style.transition = "0ms";
+          slideList.style.transform = `translateX(-${slideWidth}px)`;
+        }, slideSpeed);
+        currIndex = -1;
+      }
+      currSlide.classList.remove("slide_active");
+      currSlide = panels[++currIndex];
+      currSlide.classList.add("slide_active");
     });
-    mallEventSlide.innerHTML = panels;
+
+    prevButton.addEventListener("click", () => {
+      if (currIndex >= 0) {
+        slideList.style.transition = slideSpeed + "ms";
+        slideList.style.transform = `translateX(-${slideWidth * currIndex}px)`;
+      }
+
+      if (currIndex === 0) {
+        setTimeout(() => {
+          slideList.style.transition = "0ms";
+          slideList.style.transform = `translateX(-${slideWidth * slideLen}px)`;
+        }, slideSpeed);
+        currIndex = slideLen;
+      }
+      currSlide.classList.remove("slide_active");
+      currSlide = panels[--currIndex];
+      currSlide.classList.add("slide_active");
+    });
   });
--- shopping/public/javascripts/slide.js
+++ shopping/public/javascripts/slide.js
@@ -1,60 +1,73 @@
-const slideList = document.querySelector(".slide_list");
-const panels = document.querySelectorAll(".panel");
-const prevButton = document.querySelector(".btn_prev");
-const nextButton = document.querySelector(".btn_next");
-const slideLen = panels.length;
-const slideWidth = 485;
-const slideSpeed = 300;
-const startNum = 0;
-
-slideList.style.width = `${slideWidth * (slideLen + 2)}px`;
-
-let firstChild = slideList.firstElementChild;
-let lastChild = slideList.lastElementChild;
-let clonedFirst = firstChild.cloneNode(true);
-let clonedLast = lastChild.cloneNode(true);
-
-// add cloned slides
-slideList.appendChild(clonedFirst);
-slideList.insertBefore(clonedLast, firstChild);
-slideList.style.transform = `translateX(-${slideWidth * (startNum + 1)}px)`;
-
-let currIndex = startNum;
-let currSlide = panels[currIndex];
-currSlide.classList.add("active");
-
-nextButton.addEventListener("click", () => {
-  if (currIndex <= slideLen - 1) {
-    slideList.style.transition = `${slideSpeed}ms`;
-    slideList.style.transform = `translateX(-${slideWidth * (currIndex + 2)}px)`;
-  }
-  if (currIndex === slideLen - 1) {
-    setTimeout(() => {
-      slideList.style.transition = "0ms";
-      slideList.style.transform = `translateX(-${slideWidth}px)`;
-    }, slideSpeed);
-    currIndex = -1;
-  }
-  currSlide.classList.remove("active");
-  currSlide.classList.add("hidden");
-  currSlide = panels[++currIndex];
-  currSlide.classList.add("active");
-});
-
-prevButton.addEventListener("click", () => {
-  if (currIndex >= 0) {
-    slideList.style.transition = `${slideSpeed}ms`;
-    slideList.style.transform = `translateX(-${slideWidth * currIndex}px)`;
+export default class Slide {
+  constructor(panels, slideList, prevButton, nextButton, width, speed) {
+    this.panels = panels;
+    this.slideList = slideList;
+    this.prevButton = prevButton;
+    this.nextButton = nextButton;
+    this.slideWidth = width;
+    this.slideSpeed = speed;
+    this.currIndex = 0;
   }
 
-  if (currIndex === 0) {
-    setTimeout(() => {
-      slideList.style.transition = "0ms";
-      slideList.style.transform = `translateX(-${slideWidth * slideLen}px)`;
-    }, slideSpeed);
-    currIndex = slideLen;
+  init() {
+    this.slideList.style.width = `${this.slideWidth} * (${this.panels.length + 2})px`;
+
+    let firstChild = this.slideList.firstElementChild;
+    let lastChild = this.slideList.lastElementChild;
+    let clonedFirst = firstChild.cloneNode(true);
+    let clonedLast = lastChild.cloneNode(true);
+
+    // add cloned slides
+    this.slideList.appendChild(clonedFirst);
+    this.slideList.insertBefore(clonedLast, this.slideList.firstElementChild);
+
+    this.slideList.style.transform = `translateX(-${this.slideWidth}px)`;
+
+    const currSlide = this.panels[this.currIndex];
+    currSlide.classList.add("slide_active");
+    return currSlide;
   }
-  currSlide.classList.remove("active");
-  currSlide = panels[--currIndex];
-  currSlide.classList.add("active");
-});
+
+  moveToNext() {
+    const slideLen = this.panels.length;
+    let currSlide = this.init();
+    this.nextButton.addEventListener("click", () => {
+      if (this.currIndex <= slideLen - 1) {
+        this.slideList.style.transition = this.slideSpeed + "ms";
+        this.slideList.style.transform = `translateX(-${this.slideWidth * (this.currIndex + 2)}px)`;
+      }
+      if (this.currIndex === slideLen - 1) {
+        setTimeout(() => {
+          this.slideList.style.transition = "0ms";
+          this.slideList.style.transform = `translateX(-${this.slideWidth}px)`;
+        }, this.slideSpeed);
+        this.currIndex = -1;
+      }
+      currSlide.classList.remove("slide_active");
+      currSlide = this.panels[++this.currIndex];
+      currSlide.classList.add("slide_active");
+    });
+  }
+
+  moveToPrev() {
+    const slideLen = this.panels.length;
+    let currSlide = this.init();
+    this.prevButton.addEventListener("click", () => {
+      if (this.currIndex >= 0) {
+        this.slideList.style.transition = this.slideSpeed + "ms";
+        this.slideList.style.transform = `translateX(-${this.slideWidth * this.currIndex}px)`;
+      }
+
+      if (this.currIndex === 0) {
+        setTimeout(() => {
+          this.slideList.style.transition = "0ms";
+          this.slideList.style.transform = `translateX(-${this.slideWidth * slideLen}px)`;
+        }, this.slideSpeed);
+        this.currIndex = slideLen;
+      }
+      currSlide.classList.remove("slide_active");
+      currSlide = this.panels[--this.currIndex];
+      currSlide.classList.add("slide_active");
+    });
+  }
+}
