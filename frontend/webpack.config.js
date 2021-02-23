65360d1ca021423549a2d8bf69e9574a5f07417d
Jaeuk-YOO <ryou9305@naver.com>
2021-02-23 19:08:14 +0900

    [ADD] webpack setup style 관련 loader 추가

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,14 +1,30 @@
 import path from "path"
+const port = process.env.PORT || 3030;
 // const path = require("path")
 
 const webpackConfig = {
   mode: "development",
+  
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader", // Creates `style` nodes from JS strings
+          "css-loader", // Translates CSS into CommonJS
+          "sass-loader", // Compiles Sass to CSS
+        ],
+      },
+      
+    ],
+  },
+
   entry: {
     index: "./frontend/public/index.js",
   },
   output: {
     filename: "[name].js",
-    path: path.resolve("./dist"),
+    path: path.resolve("./build/frontend"),
   },
 }
 export default webpackConfig;
