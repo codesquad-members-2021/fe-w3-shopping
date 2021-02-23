c2b25f6cb6501b894d8d8ebf7d54b73eaf50a86a
reminderclock <reminderclock@naver.com>
2021-02-23 12:31:05 +0900

    chore: 폴더구조 변경

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.listen(3000, function() {
     console.log('start!!! express server on port 3000');
 });
 
+// for(let i=0; i<100; i++) {
+//     console.log(i);
+// }
+
 app.use(express.static('public'));
 
 app.get('/', function(req,res) {
