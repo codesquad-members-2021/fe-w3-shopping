0d6c081fc89a667deb27d431d9dd341066136aab
1nk_0 <>
2021-02-25 05:22:46 +0900

    ADD main sercion HTML
    * ADD ejs main section
    * ADD SCSS main section

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const i_router =require('./api/index');
 const app =  express();
 app.locals.homeContents = require('./api/homeContents.json');
+app.locals.planningEvent = require('./api/planningEvent.json');
 
 app.set("view engine","ejs");
 app.engine('ejs',require('ejs').renderFile);
