2643e2cb2a9679f21ddf30a82ff2f1ee1f8637e8
SEONKYU\difke <somedaycode@gmail.com>
2021-02-24 18:56:28 +0900

    data 라우트 경로 수정
    homContents.json => data/1
    planningEvent.json => data/2

--- routes/data.js
+++ routes/data.js
@@ -3,9 +3,17 @@ const router = express.Router();
 const path = require('path');
 const fs = require('fs');
 
-router.get('/', function (req, res, next) {
+router.get('/1', function (req, res, next) {
   const rawdata = fs.readFileSync(
-    path.resolve(__dirname, '../public/data/homeContents.json')
+    path.join(__dirname, '../public/data/homeContents.json')
+  );
+  const data = JSON.parse(rawdata);
+  res.json(data);
+});
+
+router.get('/2', function (req, res, next) {
+  const rawdata = fs.readFileSync(
+    path.join(__dirname, '../public/data/planningEvent.json')
   );
   const data = JSON.parse(rawdata);
   res.json(data);
