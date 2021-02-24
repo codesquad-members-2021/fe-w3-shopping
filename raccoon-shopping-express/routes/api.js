47b0e55166a668eb695efc447d6df3cf5d4a5208
Juddroid <nobllizdc@gamil.com>
2021-02-24 19:52:18 +0900

    [express] 폴더 구조 파악 및 역할 공부 / api 작성 및 호출 테스트

---
+++ raccoon-shopping-express/routes/api.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+/* GET users listing. */
+router.get('/mileageList', function (req, res, next) {
+  res.contentType('text/json');
+
+  let option = {
+    root: path.join(__dirname, '../serverData'),
+  };
+
+  res.sendFile('mileageList.json', option);
+});
+
+router.get('/mallEventList', function (req, res, next) {
+  res.contentType('text/json');
+
+  let option = {
+    root: path.join(__dirname, '../serverData'),
+  };
+  res.sendFile('mallEventList.json', option);
+});
+
+module.exports = router;
