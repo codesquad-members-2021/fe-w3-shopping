e6907ab90255dd1ae4f116e5f16e174a3e8f0b54
Juddroid <nobllizdc@gamil.com>
2021-02-25 01:20:08 +0900

    [fetch api] 생애 첫 서버로부터 이미지 호출

--- raccoon-shopping-express/front/main.js
+++ raccoon-shopping-express/front/main.js
@@ -1,10 +1,9 @@
 fetch('http://localhost:3000/api/mileageList', { mode: 'cors' })
   .then((response) => {
     let data = response.json();
-
     return data;
   })
-  .then((data) => console.log(data.mileageList[0].imgurl))
+  .then((data) => pushContents(data))
   .then((status) => console.log('Request successful', status))
   .catch((error) => console.log('Request failed', error));
 
@@ -13,6 +12,7 @@ const $prevEventButton = document.querySelector('.slide--button--prev');
 const $nextEventButton = document.querySelector('.slide--button--next');
 
 const $mallEventList = document.querySelector('#mallEventList');
+const $topMileageSlide = document.querySelector('#topMileageSlide');
 
 $mileageEventSlide.addEventListener('mouseover', overEventSlider);
 $mileageEventSlide.addEventListener('mouseout', outEventSlider);
@@ -25,4 +25,27 @@ function overEventSlider(e) {
 function outEventSlider(e) {
   $prevEventButton.querySelector('.ico--prev__slide--hover').classList.replace('ico--prev__slide--hover', 'ico--prev');
   $nextEventButton.querySelector('.ico--next__slide--hover').classList.replace('ico--next__slide--hover', 'ico--next');
+}
+
+// slide 내부 동적으로 그려보기
+
+function pushContents(contents) {
+  $topMileageSlide.insertAdjacentHTML('afterbegin', getMileageEventContents(contents));
+}
+
+function getMileageEventContents(jsonData) {
+  const imgurlArr = jsonData.mileageList.map((el) => el.imgurl);
+  const linkurlArr = jsonData.mileageList.map((el) => el.linkurl);
+
+  let panelDiv = ``;
+  for (let i = 0; i < imgurlArr.length; i++) {
+    panelDiv += `
+    <div class="panel">
+    <a href="${linkurlArr[i]}" class="link--event"
+      ><img src="${imgurlArr[i]}" width="485" height="340" class="img_g" alt=""
+    /></a>
+    </div>`;
+  }
+
+  return panelDiv;
 }
