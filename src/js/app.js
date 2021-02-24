f912cf15f31a8a923795979a4b9d1155e55cab09
cchoongh <cndgh49@naver.com>
2021-02-24 23:16:38 +0900

    Do many things..
    * added simple server
    * implemented main event slide's controller UI
    * enhanced '.scss'

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,13 @@
 import '../scss/style.scss';
+
+const SERVER = 'http://localhost';
+const PORT = 3000;
+
+const requestURL = `${SERVER}:${PORT}/`;
+const homeContentsJSON = 'json/homeContents.json';
+const planningEventJSON = 'json/planningEvent.json';
+
+// fetch(requestURL + planningEventJSON)
+//   .then((res) => res.json())
+//   .then((json) => console.log(JSON.stringify(json)))
+//   .catch(console.error);
