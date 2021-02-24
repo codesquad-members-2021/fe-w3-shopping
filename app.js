1f04f166e5cf4ee70c9282665530c703a7084fe1
SEONKYU\difke <somedaycode@gmail.com>
2021-02-24 23:09:40 +0900

    scss 폴더와 css 폴더를 분리함
    app.js에서 미들웨어 dest와 src의 경로를 바꿔주니 해결되었다.
    이걸로 많은 시간을 잡아먹었는데 index.ejs 파일의 경로도 바꾸어 주어야했다.
    index.ejs는 css 파일을 불러오는데 그 전에 app.js는 style.scss를 지정된 경로에 css로 컴파일을 하는 것이였다.

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   sassMiddleware({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
+    src: path.join(__dirname, 'public/scss'),
+    dest: path.join(__dirname, 'public/css'),
     indentedSyntax: false, // true = .sass and false = .scss
     sourceMap: true,
   })
