03ac22f85607b5912acacf6775141880824fe676
SEONKYU\difke <somedaycode@gmail.com>
2021-02-24 18:48:23 +0900

    json파일을 읽어올 data 경로 라우팅 설정

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const sassMiddleware = require('node-sass-middleware');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const dataRouter = require('./routes/data');
 
 const app = express();
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/data', dataRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
