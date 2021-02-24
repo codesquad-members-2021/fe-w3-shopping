e3004adb0b6c0225fbf7b7470af12108878a2c9c
Jungwon Lee <leejungwon96@gmail.com>
2021-02-24 20:23:33 +0900

    [test] fetch data and add it in html with js

--- shoppingHow/public/javascripts/slide.js
+++ shoppingHow/public/javascripts/slide.js
@@ -1,7 +1,14 @@
-// function loadItems() {
-//   return fetch("http://localhost:3000/homeContents.json")
-//     .then((response) => response.json())
-//     .then((json) => console.log(json));
-// }
+function loadItems() {
+  let datas = "";
+  fetch("http://localhost:3000/homeContents.json")
+    .then((response) => response.json())
+    .then((data) => {
+      datas = data.contents[0].eventContent.imgurl;
+      console.log(datas);
+      let slide = document.createElement("li");
+      slide.innerHTML = `<img src=${datas}></img>`;
+      document.querySelector(".slide_list").appendChild(slide);
+    });
+}
 
-// loadItems();
+loadItems();
