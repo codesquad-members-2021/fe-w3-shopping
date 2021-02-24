a0b233ed32fb2dd61eb05b802c7d527fcc54f7a4
Juddroid <nobllizdc@gamil.com>
2021-02-24 23:13:29 +0900

    [기능구현] mileage event list slider hover 기능 추가

--- raccoon-shopping-express/front/main.js
+++ raccoon-shopping-express/front/main.js
@@ -8,5 +8,19 @@ fetch('http://localhost:3000/api/mileageList', { mode: 'cors' })
   .then((status) => console.log('Request successful', status))
   .catch((error) => console.log('Request failed', error));
 
-const mileageSlide = document.querySelector('#mileageSilde');
-console.log(mileageSlide);
+const $mileageEventSlide = document.querySelector('.event--slide');
+const $prevEventButton = document.querySelector('.slide--button--prev');
+const $nextEventButton = document.querySelector('.slide--button--next');
+
+$mileageEventSlide.addEventListener('mouseover', overEventSlider);
+$mileageEventSlide.addEventListener('mouseout', outEventSlider);
+
+function overEventSlider(e) {
+  $prevEventButton.querySelector('.ico--prev').classList.replace('ico--prev', 'ico--prev__slide--hover');
+  $nextEventButton.querySelector('.ico--next').classList.replace('ico--next', 'ico--next__slide--hover');
+}
+
+function outEventSlider(e) {
+  $prevEventButton.querySelector('.ico--prev__slide--hover').classList.replace('ico--prev__slide--hover', 'ico--prev');
+  $nextEventButton.querySelector('.ico--next__slide--hover').classList.replace('ico--next__slide--hover', 'ico--next');
+}
