bb1d7c61d624fa6877961045cda2a7c5b3848f5e
17-sss <xzxking17@gmail.com>
2021-02-24 15:59:17 +0900

    [Add] data & data router, [Refactor] Dir Structure

--- data/more.js
+++ data/more.js
@@ -1,8 +1,8 @@
-const more1 = require('./json/more1.json');
-const more2 = require('./json/more2.json');
-const more3 = require('./json/more3.json');
-const more4 = require('./json/more4.json');
-const more5 = require('./json/more5.json');
+const more1 = require('./more/more1.json');
+const more2 = require('./more/more2.json');
+const more3 = require('./more/more3.json');
+const more4 = require('./more/more4.json');
+const more5 = require('./more/more5.json');
 
 const moreData = [...more1.contents, ...more2.contents, ...more3.contents, ...more4.contents, ...more5.contents];
 
