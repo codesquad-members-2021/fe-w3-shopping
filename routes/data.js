8a2983e3e28cd5b69b918cb62526522b72bdd7fc
SEONKYU\difke <somedaycode@gmail.com>
2021-02-24 20:27:54 +0900

    data 라우트 불필요한 코드 삭제
    파일을 읽고 또 따로 res.json을 할 필요가 없었다.
    그저 파일만 보내면 되기 때문에 res.sendFile을 이용하여 코드를 리팩토링 하였다.

--- routes/data.js
+++ routes/data.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const fs = require('fs');
 
 router.get('/1', function (req, res, next) {
-  const rawdata = fs.readFileSync(
-    path.join(__dirname, '../public/data/homeContents.json')
-  );
-  const data = JSON.parse(rawdata);
-  res.json(data);
+  res.sendFile(path.join(__dirname, '../public/data/homeContents.json'));
 });
 
 router.get('/2', function (req, res, next) {
-  const rawdata = fs.readFileSync(
-    path.join(__dirname, '../public/data/planningEvent.json')
-  );
-  const data = JSON.parse(rawdata);
-  res.json(data);
+  res.sendFile(path.join(__dirname, '../public/data/planningEvent.json'));
 });
 
 module.exports = router;
