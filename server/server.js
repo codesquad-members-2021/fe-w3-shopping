cc3d5e66a0dd5a73ab3dd256a02df62798fbac2e
kowoohyuk <kowoohyuk91@gmail.com>
2021-02-25 10:12:09 +0900

    json 추가 및 server.js 수정

--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
 const app = require("express")();
 const port = 3000;
 const resFile = require('./response.json');
-const { carousel, box } = resFile;
+const { best, event, carousel, box } = resFile;
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, X-Requested-With");
   next();
- });
+});
 
 app.get('/item', (req, res) => {
   const { query : { type, page, count } } = req;
   res.status(200);
+  if(type === 'best') {
+    res.json(best);
+    return;
+  }
+  if(type === 'event') {
+    res.json(event);
+    return;
+  }
   if(type === 'carousel') {
     res.json(carousel);
     return;
@@ -37,8 +45,6 @@ app.get('/length', (req, res) => {
   });
   res.end();
 });
-
-
 
 app.listen(port, () => {
   console.log(`Server Loaded, http://localhost:${port}`);
