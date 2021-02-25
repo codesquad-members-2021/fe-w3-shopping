fe9eb20cf2fecdf75f750b9af7b89fc0937d9b18
SEONKYU\difke <somedaycode@gmail.com>
2021-02-25 12:34:10 +0900

    class Silder 추가함
    클릭 이벤트를 테스트 해보았다.

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,34 @@
 import { LoadItem } from './loadItem.js';
 
+class Slider {
+  constructor() {
+    this.silder = document.querySelector('.slide');
+  }
+
+  onEvents() {
+    this.silder.addEventListener('click', this.clickArrowBtnHandler);
+  }
+
+  clickArrowBtnHandler(e) {
+    // anchor tag 눌림 방지
+    e.preventDefault();
+    console.log('버튼이 클릭되었습니다.');
+  }
+}
+
 const loadItems = () => {
   const loadItem = new LoadItem();
   loadItem.showImgContents();
 };
 
+const excuteSlide = () => {
+  const slider = new Slider();
+  slider.onEvents();
+};
+
 const main = () => {
   loadItems();
+  excuteSlide();
 };
 
 main();
