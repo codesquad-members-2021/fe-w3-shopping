b781d5fc48f95f42f0160e22d7efd571f62eb37d
SEONKYU\difke <somedaycode@gmail.com>
2021-02-25 12:12:22 +0900

    main.js에 loadItem.js파일 import

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,14 @@
-fetch('http://localhost:3000/data/1')
-  .then((response) => response.json())
-  .then((json) => console.log(json))
-  .catch((error) => console.log('에러입니다', error));
+import { LoadItem } from './loadItem.js';
+
+const loadItems = () => {
+  const loadItem = new LoadItem();
+  loadItem.showImgContents();
+};
+
+const main = () => {
+  loadItems();
+};
+
+main();
+
+// console.log('1');
