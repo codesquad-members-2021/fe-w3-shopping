e5b668b128a0dabfcf6dbbed85344eb007b2f920
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-02-25 23:44:07 +0900

    Add : 쇼핑 메인 아이템리스트 기능 추가
    * JSON 파일에서 랜덤으로 데이터를 불러와서 item_list 라는 ul 태그에 추가한다.

---
+++ shopping/public/javascripts/itemList.js
@@ -0,0 +1,35 @@
+class itemList {
+    constructor() {
+        this.itemUl = document.querySelector(".item_list")
+    }
+
+    eventHandler() {
+        window.addEventListener("load", this.loadRandomImage.bind(this));
+    }
+
+    getRandomIdx(m, n) {
+        return m + Math.floor((n - m + 1) * Math.random());
+    }
+
+    loadRandomImage() {
+        for (let i = 0; i < 5; i++) {
+
+            fetch("http://localhost:3000/homeData.json")
+                .then(response => response.json())
+                .then(json => {
+                    const data = json.mallEventList;
+                    const index = this.getRandomIdx(0, data.length - 1);
+                    this.fillItemUl(data[index].imgurl, data[index].text, data[index].text2);
+                });
+
+        }
+    }
+
+    fillItemUl(img, title, text) {
+        const li = document.createElement("li");
+        li.innerHTML = `<img class = "item_list_img" src = ${img}> <br> ${title} <br> ${text}`
+        this.itemUl.appendChild(li);
+    }
+}
+
+new itemList().eventHandler();
