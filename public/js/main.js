e2cfb768a1d7386d91656e4da09b3bd608dbdefd
SEONKYU\difke <somedaycode@gmail.com>
2021-02-25 20:07:27 +0900

    모듈화에 따른 클래스 분리 작업

--- public/js/main.js
+++ public/js/main.js
@@ -1,41 +1,16 @@
 import { LoadItem } from './loadItem.js';
-
-class Slider {
-  constructor() {
-    this.silder = document.querySelector('.slide');
-  }
-
-  onEvents() {
-    this.silder.addEventListener('click', this.clickArrowBtnHandler);
-  }
-
-  clickArrowBtnHandler(e) {
-    const slideImgs = document.querySelectorAll('.slide-img');
-    const imgContainer = document.querySelector('.main-top-slide-imgs');
-
-    if (e.target.closest('.arrow-btn-right')) {
-      slideImgs.forEach((img) => {
-        img.classList.add('slide-move');
-      });
-    }
-
-    if (e.target.closest('.arrow-btn-left')) {
-      console.log('왼쪽 버튼클릭');
-      slideImgs.forEach((img) => {
-        img.style.transform = 'translate3d(35rem,0,0)';
-      });
-    }
-  }
-}
+import { Slider } from './slider.js';
 
 const loadItems = () => {
   const loadItem = new LoadItem();
   loadItem.showImgContents();
+  loadItem.onEvents();
 };
 
 const excuteSlide = () => {
   const slider = new Slider();
   slider.onEvents();
+  slider.slideAutomatically();
 };
 
 const main = () => {
