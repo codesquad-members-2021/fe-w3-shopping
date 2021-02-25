acdbf987c83f276c007861f29f1fd3289d7b7bbd
SEONKYU\difke <somedaycode@gmail.com>
2021-02-25 12:08:43 +0900

    css랑 scss 폴더를 나누니깐 에러가 발생
    다시 하나로 합쳐주었다.
    처음 localhost에 들어갔을 때는 문제가 없지만 새로고침을 하면 문제가 발생하는것을 알 수 있다.
    왜 그런지 모르겠음

--- routes/data.js
+++ routes/data.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-router.get('/1', function (req, res, next) {
+router.get('/homeContents.json', function (req, res, next) {
   res.sendFile(path.join(__dirname, '../public/data/homeContents.json'));
 });
 
-router.get('/2', function (req, res, next) {
+router.get('/planningEvent.json', function (req, res, next) {
   res.sendFile(path.join(__dirname, '../public/data/planningEvent.json'));
 });
 
