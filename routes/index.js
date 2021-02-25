b51ef9ec73b6fbddee0438bbaa82c98408eb8dc4
younggil <zelord.kwoun@gmail.com>
2021-02-25 21:57:43 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
+// var bodyParser = require('body-parser');
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:true}));
 
 /* GET home page. 
 router.get('/', function(req, res, next) {
@@ -11,4 +16,12 @@ router.get('/', function(req, res, next) {
   res.render('index.html');
 });
 
+router.get('/form', function(req, res ) {
+  res.render('form.html');
+});
+
+
+router.post('/post_email', function(req,res){
+  res.send("<h1> welcome " + req.body.email + "</h1>")
+})
 module.exports = router;
