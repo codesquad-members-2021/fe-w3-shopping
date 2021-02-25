1c12177786e945ee58bb9a926500c7175796da1a
Hannah Park <ha3158987@gmail.com>
2021-02-25 21:49:54 +0900

    Refactor: replace html structure to templating

--- shopping_how/public/javascripts/index.js
+++ shopping_how/public/javascripts/index.js
@@ -1,4 +1,12 @@
-import { FileReader } from "./painter.js";
+/* --------------------------------------------------------------------- */
+/* -----▶︎▶︎▶︎ Main 클래스: 프로그램을 시작시키고, 클래스들 간의 매개체가 된다. ◀︎◀︎◀︎-------*/
+/* --------------------------------------------------------------------- */
+
+/*
+- [ ] Template 클래스 생성해서 templating으로 DOM 채우기 (첫화면)
+*/
+
+import { FileReader } from "./fileReader.js";
 
 class Main {
     constructor(fileReader){
