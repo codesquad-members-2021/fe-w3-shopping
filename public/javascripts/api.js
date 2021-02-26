2d37b5d452bad56f015b34841fa4a6d39356763b
damilog <dami.k.dev@gmail.com>
2021-02-26 17:25:09 +0900

    feat : express 에서 받아온 이미지로 캐러셀 랜더링하는 기능 추가

--- public/javascripts/api.js
+++ public/javascripts/api.js
@@ -1,9 +1,31 @@
-export function runAPI() {
-  let data;
-  const promise = fetch("http://localhost:3000/image")
-    .then(response => response.json())
-    .then(json => {
-      data = json;
-      console.log(data);
-    });
+import cloneContent from "./cloneContent.js";
+import TopCarouselUI from "./TopCarouselUI.js";
+export default class API {
+  constructor() {
+    this.requestImg();
+  }
+  requestImg() {
+    fetch("http://localhost:3000/image")
+      .then(response => response.json())
+      .then(json => {
+        this.parseJson(json["mileageList"]);
+        cloneContent();
+        const topCarouselUI = new TopCarouselUI();
+        q;
+      });
+  }
+
+  parseJson(data) {
+    const $carousel = document.querySelector(".item__carousel-wrap");
+    for (const value of data) {
+      let template = `<div class="carousel-content">
+      <a href="#"" >
+        <img
+          src=${JSON.stringify(value["imgurl"])}
+      /></a>
+    </div>`;
+
+      $carousel.insertAdjacentHTML("beforeend", template);
+    }
+  }
 }
