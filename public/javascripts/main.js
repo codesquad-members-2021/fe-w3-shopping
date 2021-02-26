076c0a38c5f64e1a1585e5b2e7f886d531ab381f
damilog <dami.k.dev@gmail.com>
2021-02-26 15:48:28 +0900

    feat : first 캐러셀, last 캐러셀 복제를 위한 cloneContent 모듈 추가

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,20 @@
-import { runAPI } from "./api.js";
+import API from "./api.js";
 import TopCarouselUI from "./TopCarouselUI.js";
-//runAPI();
+import cloneContent from "./cloneContent.js";
+// const api = new API();
 
+const carouselSlide = document.querySelector(".item__carousel-wrap");
+
+//counter
+
+// const cloneContent = () => {
+//   const clonedFirstChild = carouselSlide.firstElementChild.cloneNode(true);
+//   clonedFirstChild.id = "firstClone";
+//   const clonedLastChild = carouselSlide.lastElementChild.cloneNode(true);
+//   clonedLastChild.id = "lastClone";
+//   carouselSlide.insertBefore(clonedFirstChild, null);
+//   carouselSlide.insertBefore(clonedLastChild, carouselSlide.firstElementChild);
+// };
+
+cloneContent();
 const topCarouselUI = new TopCarouselUI();
