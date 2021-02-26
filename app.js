49b599ef172f600c1d58f7fca69b248ea3da323b
dudn1933 <75162982+dudn1933@users.noreply.github.com>
2021-02-26 20:37:55 +0900

    express 서버에서 사진가지고오기

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors'); // CORS 미들웨어 객체 선언
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const imageRouter = require('./routes/image');
 
 const app = express();
 
@@ -13,14 +15,16 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/image', imageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
