c5d8b99bf0295bdbddcc84885a54726fc3d1c4fb
Juddroid <nobllizdc@gamil.com>
2021-02-27 04:27:08 +0900

    [HTML] Mall event slide 동적으로 그리기 완료

--- raccoon-shopping-express/front/main.js
+++ raccoon-shopping-express/front/main.js
@@ -34,7 +34,10 @@ class FetchAPI {
     fetch(this.url.mallEventList)
       .then((response) => response.json())
       .then((data) => {
-        const mallEventSection = new MallEventSection(data);
+        const mallEventSection = new MallEventSection(data.mallEventList);
+        mallEventSection.getMallEventPanel();
+        mallEventSection.getMallEventPanel();
+        mallEventSection.getMallEventPanel();
         return data;
       })
       .then((status) => console.log(this.req.sucess, status.code))
@@ -280,48 +283,49 @@ eventSliderListener.init();
 class MallEventSection {
   constructor(data) {
     this.data = data;
+    // console.log(this.data);
+    this.slide = document.querySelector('#mallEventSlide');
+    this.count = 0;
   }
 
   getMallEventPanel() {
-    return `
+    const ITEM_BOX = 5;
+    let listItems = ``;
+    for (let i = 0; i < ITEM_BOX; i++) {
+      listItems += this.getMallEventItem(this.data[this.count]);
+      this.count++;
+    }
+    let panel = this.slide.insertAdjacentHTML(
+      'beforeend',
+      `
     <div class="panel" aria-hidden="true">
-    <ul class="list--item">
-    </ul>
+      <ul class="list--item">
+      ${listItems}
+      </ul>
     </div>
-    `;
+    `
+    );
+    return panel;
   }
-  getMallEventItem() {
-    const $listItem = document.querySelector('.list--item');
+  getMallEventItem(list) {
+    const { dataseq, imgurl, text, text2, linkurl } = list;
+
+    return `
+    <li class=${dataseq}>
+    <a href="${linkurl} class="link--product"
+      ><span class="info--thumb"
+        ><img
+          src=${imgurl}
+          width="200"
+          height="200"
+          class="imgage--top"
+          alt=${text} /></span
+      ><strong class="info--title">${text}</strong><span class="info--txt">${text2}</span
+      ><span class="ico--background2 ico--theme">테마</span></a
+    >
+  </li>
+    `;
   }
   pushItemData() {}
   mallEventCarousel() {}
-}
-
-function getMallEventPanel() {
-  return `
-<div class="panel" aria-hidden="true">
-<ul class="list--item">
-</ul>
-</div>
-`;
-}
-
-function getMallEventItem(list) {
-  const { dataseq, imgurl, text, text2, linkurl } = list;
-
-  return `
-  <li class=${dataseq}>
-  <a href="${linkurl} class="link--product"
-    ><span class="info--thumb"
-      ><img
-        src=${imgurl}
-        width="200"
-        height="200"
-        class="imgage--top"
-        alt=${text} /></span
-    ><strong class="info--title">${text}</strong><span class="info--txt">${text2}</span
-    ><span class="ico--background2 ico--theme">테마</span></a
-  >
-</li>
-  `;
 }
