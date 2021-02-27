cd9e1fb504d780adb7315a16df20077c80dbc451
cchoongh <cndgh49@naver.com>
2021-02-27 15:42:12 +0900

    Add 'genEl' util function and Refactor table
    * added 'genEl' function to use to create DOM element easily
    * added 'ITable' interface
    * refactored 'EvtCardTable' class

--- src/js/table.js
+++ src/js/table.js
@@ -1,48 +1,61 @@
+import { _ } from './util.js';
 import { createEvtCardListElFrom } from './common-creator.js';
 
-export class EvtCardTable {
-  constructor({ target, jsonList, columnCnt }) {
+class ITable {
+  constructor(target, columnCnt) {
     this.$target = target;
+    this.columnCnt = columnCnt;
+  }
+
+  addRow(itemCnt = this.columnCnt) { throw new Error('Abstact method!'); }
+}
+
+export class EvtCardTable extends ITable {
+  constructor({ target, jsonList, columnCnt }) {
+    super(target, columnCnt);
     this.$seeMoreBtn;
     this.jsonList = jsonList;
-    this.columnCnt = columnCnt;
     this.nextJsonIdxToLoad = 0;
   }
 
   init() {
     this.initSeeMoreBtn();
-    this.addEvtCardList(this.columnCnt);
+    this.addRow();
     this.onEvents();
   }
 
-  addEvtCardList(itemCnt) {
-    const $evtCardList = createEvtCardListElFrom(this.jsonList.slice(this.nextJsonIdxToLoad, this.nextJsonIdxToLoad + itemCnt));
-    this.$target.insertBefore($evtCardList, this.$seeMoreBtn);
-    this.nextJsonIdxToLoad += itemCnt;
-
-    if (this.nextJsonIdxToLoad >= this.jsonList.length)
-      this.$seeMoreBtn.hidden = true;
-
-    const hilightTxt = this.$seeMoreBtn.querySelector('.hilight-txt');
-    hilightTxt.innerText = `${Number(hilightTxt.innerText) + itemCnt}`;
-  }
-
   initSeeMoreBtn() {
-    this.$seeMoreBtn = document.createElement('DIV');
-    this.$seeMoreBtn.classList.add('evt-card-table__see-more-btn');
-    this.$seeMoreBtn.innerHTML =
-      `<span class="main-txt">더보기
-      <span>&#40;</span>
-      <span class="hilight-txt">0</span>
-      <span>&#47;&#160;${this.jsonList.length}건&#41;</span>
-      </span>`
+    this.$seeMoreBtn = _.genEl({
+      tagName: 'DIV',
+      classNames: ['evt-card-table__see-more-btn'],
+      template: `<span class="main-txt">더보기
+        <span>&#40;</span>
+        <span class="hilight-txt">${this.columnCnt}</span>
+        <span>&#47;&#160;${this.jsonList.length}건&#41;</span>
+        </span>`,
+    });
+    
     this.$target.appendChild(this.$seeMoreBtn);
   }
 
+  addRow(itemCnt = this.columnCnt) {
+    const $evtCardList = createEvtCardListElFrom(this.jsonList.slice(this.nextJsonIdxToLoad, this.nextJsonIdxToLoad + itemCnt));
+    this.$target.insertBefore($evtCardList, this.$seeMoreBtn);
+    this.nextJsonIdxToLoad += itemCnt;
+  }
+
   onEvents() {
-    this.$seeMoreBtn.addEventListener('click', () => {
-      console.log('clk!');
-      this.addEvtCardList(this.columnCnt - 1);
-    });
+    this.$seeMoreBtn.addEventListener('click', this.onClickSeeMorebtn.bind(this));
+  }
+
+  onClickSeeMorebtn() {
+    const itemCnt = this.columnCnt - 1;
+    this.addRow(itemCnt);
+
+    if (this.nextJsonIdxToLoad >= this.jsonList.length)
+      this.$seeMoreBtn.hidden = true;
+    
+    const hilightTxt = _.$('.hilight-txt', this.$seeMoreBtn);
+    hilightTxt.innerText = `${Number(hilightTxt.innerText) + itemCnt}`;
   }
 }
--- src/js/util.js
+++ src/js/util.js
@@ -1,4 +1,14 @@
 export const _ = {
   $: (selector, base = document) => base.querySelector(selector),
   $All: (selector, base = document) => base.querySelectorAll(selector),
+  genEl: ({ tagName, classNames, template, attributes} ) => {
+    const $el = document.createElement(tagName);
+    $el.classList.add(...classNames);
+    $el.innerHTML = template;
+
+    for (const [k, v] in attributes)
+      $el.setAttribute(k, v);
+
+    return $el;
+  },
 };
