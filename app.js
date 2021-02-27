dbccca0296276607ff93455d6f528a287e8ee325
swing <69034766+swing-park@users.noreply.github.com>
2021-02-27 23:50:48 +0900

    Modify screen rendering scheme (#87)

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const { log } = console;
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use((req, res) => res.sendStatus(404));
+
+app.get('/rightpannel1', (req, res) => {
+    res.sendFile(__dirname + "/src/img/rightpannel1.png")
+})
+app.get('/rightpannel2', (req, res) => {
+    res.sendFile(__dirname + "/src/img/rightpannel2.png")
+})
+app.get('/rightpannel3', (req, res) => {
+    res.sendFile(__dirname + "/src/img/rightpannel3.png")
+})
 
 app.listen(port, () => log(`The server is listening at ${port}`));
--- src/js/carouselUI.js
+++ src/js/carouselUI.js
@@ -1,9 +1,9 @@
 export default class CarouselUI {
     constructor() {
         this.carouselUI = document.querySelector('.carouselUI');
-        this.imgurl1 = "http://localhost:8080/rightpannel1.05c10acb.png";
-        this.imgurl2 = "http://localhost:8080/rightpannel2.9749f624.png";
-        this.imgurl3 = "http://localhost:8080/rightpannel3.a20f5d1e.png";
+        this.imgurl1 = "http://localhost:8080/rightpannel1";
+        this.imgurl2 = "http://localhost:8080/rightpannel2";
+        this.imgurl3 = "http://localhost:8080/rightpannel3";
     }
 
     makeImageDOM(imgurl) {
@@ -17,10 +17,12 @@ export default class CarouselUI {
         fetch(this.imgurl1)
             .then(fetch(this.imgurl2))
             .then(fetch(this.imgurl3))
-            .then(this.makeImageDOM(this.imgurl3))
-            .then(this.makeImageDOM(this.imgurl1))
-            .then(this.makeImageDOM(this.imgurl2))
-            .then(this.makeImageDOM(this.imgurl3))
-            .then(this.makeImageDOM(this.imgurl1))
+            .then(
+                this.makeImageDOM(this.imgurl3),
+                this.makeImageDOM(this.imgurl1),
+                this.makeImageDOM(this.imgurl2),
+                this.makeImageDOM(this.imgurl3),
+                this.makeImageDOM(this.imgurl1),
+            )
     }
 }
