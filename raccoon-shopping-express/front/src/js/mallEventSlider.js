9985876c73e4c04aa4982f993747479d924cfc45
Juddroid <nobllizdc@gamil.com>
2021-02-28 22:09:44 +0900

    [2초 버튼 추가]
    1. 2초가 너무 길어서 0.7초 후에 타이머가 작동하도록 했다.
    2. 누르고 있으면 계속해서 스크롤 되도록 했다.
    3. 마우스를 떼면 타이머 삭제 후 정지
    4. 최초 mousedown은 클릭으로 간주하고, 한번 넘어가도록 했다.

--- raccoon-shopping-express/front/src/js/mallEventSlider.js
+++ raccoon-shopping-express/front/src/js/mallEventSlider.js
@@ -2,9 +2,14 @@ class MallEventSlider {
   constructor(target) {
     this.target = target;
     this.slide = document.querySelector('#mallEventSlide');
+    this.longClickTimer = -1;
+    this.prevTimer = -1;
+    this.nextTimer = -1;
   }
   addEvent() {
-    this.target.forEach((el) => el.addEventListener('click', this.mallEventSlider.bind(this)));
+    // this.target.forEach((el) => el.addEventListener('click', this.mallEventSlider.bind(this)));
+    this.target.forEach((el) => el.addEventListener('mousedown', this.longClickSlide.bind(this)));
+    this.target.forEach((el) => el.addEventListener('mouseup', this.clearLongClickSlide.bind(this)));
   }
 
   mallEventSlider(e) {
@@ -18,9 +23,50 @@ class MallEventSlider {
       this.clickNext();
     }
   }
+
+  mallEvent2Sliders(e) {
+    const prev = () => e.target.classList.contains('button--prev') || e.target.parentNode.classList.contains('button--prev');
+    const next = () => e.target.classList.contains('button--next') || e.target.parentNode.classList.contains('button--next');
+
+    if (prev()) {
+      this.prevTimer = setInterval(() => {
+        this.clickPrev();
+      }, 700);
+      return (this.seconds = 0);
+    }
+
+    if (next()) {
+      this.nextTimer = setInterval(() => {
+        this.clickNext();
+      }, 700);
+      return (this.seconds = 0);
+    }
+  }
+
+  /* 
+  1. 버튼을 누른다
+  2. 타이머가 동작한다.
+  3-1. 버튼을 때었다 (2초 이전)
+    - 타이머를 클리어한다.
+  3-2. 버튼을 때었다 (2초 이후)
+    - 2개 넘어가기 액션을 한다.
+  */
+  longClickSlide(e) {
+    this.mallEventSlider(e);
+    this.longClickTimer = setTimeout(() => {
+      this.mallEvent2Sliders(e);
+    }, 700);
+  }
+  clearLongClickSlide() {
+    clearTimeout(this.longClickTimer);
+    clearTimeout(this.prevTimer);
+    clearTimeout(this.nextTimer);
+  }
+
   clickPrev() {
     const listItem = this.slide.querySelectorAll('.list--item');
     this.slide.classList.replace('slide', 'slide--click--prev');
+
     setTimeout(() => {
       const firstLast = listItem[0].lastElementChild.cloneNode(true);
       const secondLast = listItem[1].lastElementChild.cloneNode(true);
