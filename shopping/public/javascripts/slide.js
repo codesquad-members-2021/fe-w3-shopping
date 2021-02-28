2cdfd6201b2015486c0465ba3cec6f3d4f2d29b9
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-02-28 23:23:37 +0900

    Refactoring : 슬라이드 애니메이션 함수 리팩토링
    * moveRight, moveLeft 두 개로 나뉘어져 있던 함수를 move(direction) 함수 하나로 합쳤음.

--- shopping/public/javascripts/slide.js
+++ shopping/public/javascripts/slide.js
@@ -12,8 +12,12 @@ class Slider {
 
     eventHandler() {
         for(let i = 0; i < 3; i++) this.loadRandomImage(i);
-        this.rightArrow.addEventListener('click', this.moveRight.bind(this));
-        this.leftArrow.addEventListener('click', this.moveLeft.bind(this));
+        this.rightArrow.addEventListener('click', () => {
+            this.move("right");
+        });
+        this.leftArrow.addEventListener('click', () => {
+            this.move("left");
+        });
         this.comm.forEach((el, ind) => {
             el.addEventListener('mouseenter', () => {
                 this.pagingComm(el, ind);
@@ -28,19 +32,18 @@ class Slider {
         this.slider.style.transform = `translate(` + (this.sectionIndex) * -33 + `%)`;
     }
 
-    moveRight() {
-        this.sectionIndex = (this.sectionIndex < 2) ? this.sectionIndex + 1 : 0;
+    move(direction) {
+        if(direction === "right") {
+            this.sectionIndex = (this.sectionIndex < 2) ? this.sectionIndex + 1 : 0;
+        }
+        if(direction === "left") {
+            this.sectionIndex = (this.sectionIndex > 0) ? this.sectionIndex - 1 : 2;
+        }
         document.querySelector('.controls .selected').classList.remove('selected');
         this.indicatorParents.children[this.sectionIndex].classList.add('selected');
         this.slider.style.transform = `translate(` + (this.sectionIndex) * -33 + `%)`;
     }
 
-    moveLeft() {
-        this.sectionIndex = (this.sectionIndex > 0) ? this.sectionIndex - 1 : 2;
-        document.querySelector('.controls .selected').classList.remove('selected');
-        this.indicatorParents.children[this.sectionIndex].classList.add('selected');
-        this.slider.style.transform = `translate(` + (this.sectionIndex) * -33 + `%)`;
-    }
 
     getRandomNum(m, n) {
         return m + Math.floor((n - m + 1) * Math.random());
