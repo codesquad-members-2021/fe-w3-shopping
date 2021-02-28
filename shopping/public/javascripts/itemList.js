8bbad1a1ff921df27d498f69adc7467a462edad4
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-02-28 22:03:40 +0900

    Add : 아이템 리스트 더보기 기능 추가
    * 더보기 버튼 클릭하면 아이템 5개가 랜덤으로 더 로딩됌.

--- shopping/public/javascripts/itemList.js
+++ shopping/public/javascripts/itemList.js
@@ -1,38 +1,57 @@
 class itemList {
     constructor() {
-        this.itemUl = document.querySelector(".item_list")
+        this.itemList = document.querySelector(".item_list"),
+            this.showMoreBtn = document.querySelector(".show_more"),
+            this.smallContents = document.querySelector(".small_contents")
     }
 
     eventHandler() {
-        window.addEventListener("load", this.loadRandomImage.bind(this));
+        window.addEventListener("load", () => {
+            this.loadRandomItems(5);
+        });
+        this.showMoreBtn.addEventListener("click", () => {
+            this.loadRandomItems(5);
+        });
     }
 
-    getRandomIdx(m, n) {
+    getRandomNum(m, n) {
         return m + Math.floor((n - m + 1) * Math.random());
     }
 
-    loadRandomImage() {
-        for (let i = 0; i < 5; i++) {
-            const URL = "http://localhost:3000/homeData.json"
-            fetch(URL)
-                .then(response => response.json())
-                .then(json => {
-                    const data = json.mallEventList;
-                    const index = this.getRandomIdx(0, data.length - 1);
-                    this.fillItemUl(data[index].imgurl, data[index].text, data[index].text2);
-                });
+    loadRandomItems(imgCount) {
+        const URL = "http://localhost:3000/homeData.json"
+        fetch(URL)
+            .then(response => response.json())
+            .then(json => {
 
-        }
+                    const ul = document.createElement("ul");
+                    ul.classList.add("item_list");
+                    this.smallContents.appendChild(ul);
+
+                    for (let i = 0; i < imgCount; i++) {
+                        const data = json.mallEventList;
+                        const index = this.getRandomNum(0, data.length - 1);
+                        this.renderItemList(data[index].imgurl, data[index].text, data[index].text2);
+                    }
+            });
     }
 
-    fillItemUl(img, title, text) {
+
+    renderItemList(img, title, text) {
         const li = document.createElement("li");
         const div = document.createElement("div");
-        div.classList.add("item_wrapper");
+        const ul = document.querySelectorAll(".item_list");
+
+        
         li.appendChild(div);
+        div.classList.add("item_wrapper");
         div.innerHTML = `<img class = "item_list_img" src = ${img}><span class = "img_title">${title}</span><span class = "img_text">${text}</span>`
-        this.itemUl.appendChild(li);
+        
+        ul[ul.length-1].appendChild(li);
     }
+
+
+
 }
 
 new itemList().eventHandler();
