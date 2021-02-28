e84a490414bf36124e4486ff24850622efeddd56
reminderclock <reminderclock@naver.com>
2021-03-01 01:43:40 +0900

    fix: 케로셀 class형태로 변경

--- public/src/carousel.js
+++ public/src/carousel.js
@@ -1,69 +1,68 @@
+class Carousel {
+    constructor() {
+        this.mainSlide = document.querySelector(".main-banner__slide");
+        this.slideBundle = document.querySelector(".slide-bundle");
+        this.children = this.slideBundle.children;
+        this.len = this.children.length;
+        this.setTime = 500;
+        this.eventSignal = this.eventSignal();
 
+    }
+    eventSignal() {
+        this.decideClickDirection();
+        this.decideMouseoverChange();
+    }
+    decideMouseoverChange() {
+        this.mainSlide.addEventListener('mouseover', ({target}) => {
+            let targetKey = target.dataset.key;
+            if(targetKey === undefined) {
+                return;
+            }
+            let num = targetKey[targetKey.length-1]
+            if(targetKey === `list${num}` && this.children[1].firstElementChild.className !== `slide__link${num}`) {
+                this.setTime = 0;
+                if(this.children[0].firstElementChild.className === `slide__link${num}`) {
+                    this.dir = 'over-right';
+                    return this.moveContent();
+                }
+                else {
+                    this.dir = 'over-left';
+                    return this.moveContent();
+                }
+            }
+        });
+    }
 
-// loadHomeContentsData();
-const mainSlide = document.querySelector(".main-banner__slide");
-const slideBundle = document.querySelector(".slide-bundle");
-// let lastList = 
-let children = slideBundle.children;
-let len = children.length;
-// console.log(children);
-// console.log(slideBundle.lastElementChild);
-mainSlide.addEventListener('click', ({target}) => {
-    if(target.className === "fas fa-chevron-right") {
-        slideBundle.classList.toggle('right');
+    decideClickDirection() {
+        // let dir = '';
+        this.mainSlide.addEventListener('click', ({target}) => {
+            if(target.classList.contains('fa-chevron-right')) {
+                this.dir = 'right'
+                return this.moveContent();
+            }
+            else if(target.classList.contains('fa-chevron-left')) {
+                this.dir = 'left'
+                return this.moveContent();
+            }
+        })
+    }
+    moveContent() {
+        this.slideBundle.classList.toggle(this.dir);
         setTimeout(() => {
-            slideBundle.insertBefore(children[len-1],children[0]);
-            slideBundle.classList.toggle('right');
-        }, 500);
+            this.moveList(this.dir);
+            this.slideBundle.classList.toggle(this.dir);
+        }, this.setTime);
     }
-
-    else if(target.className === "fas fa-chevron-left") {
-        slideBundle.classList.toggle('left');
-        setTimeout(() => {
-            slideBundle.appendChild(slideBundle.firstElementChild);
-            slideBundle.classList.toggle('left');
-        }, 500);
-    }
-})
-
-mainSlide.addEventListener('mouseover', ({target}) => {
-    if(target.dataset.key === "list1" && children[1].firstElementChild.className !== "slide__link1") {
-        if(children[0].firstElementChild.className === "slide__link1") {
-            slideBundle.classList.toggle('over-right');
-            slideBundle.insertBefore(children[len-1],children[0]);
-            slideBundle.classList.toggle('over-right');
+    moveList() {
+        if(this.dir.includes('right')) {
+            return this.slideBundle.insertBefore(this.children[this.len-1],this.children[0]);
         }
-        else {
-            slideBundle.classList.toggle('over-left');
-            slideBundle.appendChild(slideBundle.firstElementChild);
-            slideBundle.classList.toggle('over-left');
+        else if(this.dir.includes('left')) {
+            return this.slideBundle.appendChild(this.slideBundle.firstElementChild);
         }
     }
 
-    else if(target.dataset.key === "list2" && children[1].firstElementChild.className !== "slide__link2") {
-        if(children[0].firstElementChild.className === "slide__link2") {
-            slideBundle.classList.toggle('over-right');
-            slideBundle.insertBefore(children[len-1],children[0]);
-            slideBundle.classList.toggle('over-right');
-        }
-        else {
-            slideBundle.classList.toggle('over-left');
-            slideBundle.appendChild(slideBundle.firstElementChild);
-            slideBundle.classList.toggle('over-left');
-        }
-    }
+}
 
-    else if(target.dataset.key === "list3" && children[1].firstElementChild.className !== "slide__link3") {
-        if(children[0].firstElementChild.className === "slide__link3") {
-            slideBundle.classList.toggle('over-right');
-            slideBundle.insertBefore(children[len-1],children[0]);
-            slideBundle.classList.toggle('over-right');
-        }
-        else {
-            slideBundle.classList.toggle('over-left');
-            slideBundle.appendChild(slideBundle.firstElementChild);
-            slideBundle.classList.toggle('over-left');
-        }
-    }
-})
-
+let slide = new Carousel();
+// slide.printCalo();
